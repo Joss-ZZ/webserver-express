07565e32d0863b470a247a6264425644ae42bd6a
Joss-ZZ <coderjzz@gmail.com>
2020-11-02 15:26:16 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,18 @@
+const { read } = require('fs');
+const http = require('http');
+
+
+http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'aplication/json' });
+
+        let salida = {
+            nombre: 'Jhosimar',
+            edad: 28,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
